431d1f76e7c34de310bd2249b1513b048f7b1787
davlatovdev <surojbekdavlatov65@gmail.com>
2023-11-14 19:27:49 +0500

    first commit

---
+++ JavaSctipt/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn")
+window.addEventListener("scroll", () => {
+    btn.classList.toggle("active", window.scrollY > 500)
+})
+btn.addEventListener('click', () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+})
